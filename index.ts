7fc52d73d267617e92625330a602b2c12a20fcbd
TrippleN <idlogin97@gmail.com>
2022-03-20 19:09:07 +0700

    :sparkles: Connect db, create new user and new post

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("Hello World!");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+  await prisma.$connect();
+
+  await prisma.user.create({
+    data: {
+      name: "Rich",
+      email: "hello@prisma.com",
+      posts: {
+        create: {
+          title: "My first post",
+          body: "Lots of really interesting stuff",
+          slug: "my-first-post",
+        },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
